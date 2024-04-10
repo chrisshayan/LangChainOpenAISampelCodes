c35cea87c326d8d28e63cf102f64a34149aa8a18
Chris Shayan <hamedshayan@gmail.com>
2024-04-10 10:42:55 +0700

    Create CustomerPredefinedSchema.py
    Sample script to generate test data, here an output:
    
    {
      "firstName": "Thuan",
      "lastName": "Tran",
      "address": {
        "street": "Hoang Van Thu Street",
        "city": "Da Nang",
        "country": "Vietnam",
        "zipCode": "55000"
      },
      "phoneNumber": "0907123456",
      "currentAccountNumber": "1234567890",
      "currentAccountBalance": "2500000",
      "creditCardNumber": "510510510510510",
      "creditCardLimit": "20000000",
      "debitCardNumber": "4224224224242"
    }

---
+++ ollama-testdata-generation/CustomerPredefinedSchema.py
@@ -0,0 +1,44 @@
+import requests
+import json
+import random
+
+model = "llama2"
+template = {
+    "firstName": "",
+    "lastName": "",
+    "address": {
+        "street": "",
+        "city": "",
+        "country": "",
+        "zipCode": ""
+    },
+    "phoneNumber": "",
+    "currentAccountNumber": "",
+    "currentAccountBalance": "",
+    "creditCardNumber": "",
+    "creditCardLimit": "",
+    "debitCardNumber": ""
+}
+
+prompt = (f"generate one realistically believable sample data set of a persons first name, last name, address in the "
+          f"Vietnam, phone number, current account number, balance for current account in VND, realistic VISA or "
+          f"Mastercard credit card"
+          f"number, limit for credit card using VND, and a VISA or Mastercard debit card number. \nUse the following "
+          f"template:"
+          f"{json.dumps(template)}.")
+
+
+data = {
+    "prompt": prompt,
+    "model": model,
+    "format": "json",
+    "stream": False,
+    "options": {"temperature": 2.5, "top_p": 0.99, "top_k": 100},
+}
+
+print(f"Generating a sample user")
+response = requests.post("http://localhost:11434/api/generate", json=data, stream=False)
+json_data = json.loads(response.text)
+print(json.dumps(json.loads(json_data["response"]), indent=2))
+
+# ollama pull llama2
