3ef676f3ad716188bfc3443927bd8a2de9664566
Chris Shayan <hamedshayan@gmail.com>
2024-04-15 14:52:59 +0700

    Update EmployeesDatabase.py

--- talktoyourdatabase/EmployeesDatabase.py
+++ talktoyourdatabase/EmployeesDatabase.py
@@ -36,7 +36,17 @@ template = """
                 You have access to all MySQL commands such as LAG, DESC.
         
         If the input is about number of employees of each department, then join three tables of departments, employees 
-        and dept_emp on key fields of dept_no and emp_no then count it
+        and dept_emp on key fields of dept_no and emp_no then count it.
+        
+        If the input is about salary of employee, then you can look at salaries table on specific date for from_date but if 
+        no date was mentioned then you can look at latest year using order by desc. The to_date can be only used for cases like
+        identifying when one employee has changes on his salary, for example if input is about: how many times one employee salary
+        changes, then you need to query the salaries table using emp_no and check how many rows exist that shows number of salary changes.
+    
+        If input was about department and salary of department, you need to join three tables of salaries to employees on 
+        emp_no field then also join with dept_emp on emp_no to find out which employees work on which department, then you can 
+        calculate sum the total salary of each department for all its employees aligning the from_date. To find the department name,
+        you can join back with departments on dept_no field.
         
         Question: {input}
 """
@@ -52,19 +62,14 @@ print(db.get_usable_table_names())
 print("Using local LLM, make sure you have installed Ollama (https://ollama.com/download) and have it running")
 llm = Ollama(model="openchat", temperature=0)
 
-agent_executor = create_sql_agent(llm, db=db, verbose=False, max_execution_time=50*60, agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,)
+agent_executor = create_sql_agent(llm, db=db, verbose=True, max_execution_time=80*60, agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,)
 
 with contextlib.suppress(Exception):
-    #print(agent_executor.invoke(prompt.format(input="Please summarize what you see interesting in max 300 words."))["output"])
-    # print(agent_executor.invoke(prompt.format(input="how many employees are there per each department in 2006?"))["output"])
-
     print(agent_executor.invoke(prompt.format(input="how many departments does the company have?"))["output"])
-    print(agent_executor.invoke(prompt.format(input="What is the average salary of employees for each department in 2002? "
+    print(agent_executor.invoke(prompt.format(input="how many employees are there per each department in 1986?"))["output"])
+    print(agent_executor.invoke(prompt.format(input="What is the average salary for each department in 1998? "
                                                     "Please show amount and department name."))["output"])
-    print(agent_executor.invoke(prompt.format(input="How much total salary increased comparing 1995 to 1996?"))["output"])
-    print(agent_executor.invoke(prompt.format(input="which department is the highest salary?"))["output"])
-    #print(agent_executor.invoke(prompt.format(input="What is average salary of employees who are older than 25 years?"))["output"])
-    #print(agent_executor.invoke(prompt.format(input="Which employee (employee id and full name) has highest salary in 2002?"))["output"])
-    #print(agent_executor.invoke(prompt.format(input="How many times employee number 10001 got salary increase?"))["output"])
-    #print(agent_executor.invoke(prompt.format(input="What is the average salary of employees for Finance department?"))["output"])
+    print(agent_executor.invoke(prompt.format(input="What is my employee turnover rate in 1996?"))["output"])
+    print(agent_executor.invoke(prompt.format(input="What is average salary of employees who are older than 25 years?"))["output"])
+    print(agent_executor.invoke(prompt.format(input="What is the average salary of employees for Finance department?"))["output"])
 
